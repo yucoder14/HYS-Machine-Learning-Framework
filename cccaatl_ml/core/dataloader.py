7efcdb3cf51a66c3c8301c6405742d241b57b7f1
Catherine Zou <zoucatherine7@gmail.com>
2026-07-05 10:54:35 -0500

    added dataloader.py

---
+++ cccaatl_ml/core/dataloader.py
@@ -0,0 +1,15 @@
+class Dataset:
+    def __init__(self):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self):
+        pass
+
+class TensorDataset:
+    def __init__(self):
+        pass
+
+class DataLoader:
+    def __init__(self):
+        pass 
